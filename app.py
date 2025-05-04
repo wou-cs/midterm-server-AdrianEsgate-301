78e78a795dc257e7e4581eb666baa18550af96c0
AdrianEsgate-301 <aesgate22@mail.wou.edu>
2025-05-04 14:30:10 -0700

    revision of hex

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route("/api/calcs/<int:x>", methods=["GET"])
 def Decrement_by_one_and_show_hex_number(x):
-    calculations = {"dec": x - 1, "hex": hex(x - 1)}
+    if x is not int:
+        return {"error": "Input must be an integer"}, 404
+    calculations = {"dec": x - 1, "hex": hex(x)}
     return calculations, 200
 
 if __name__ == "__main__":
