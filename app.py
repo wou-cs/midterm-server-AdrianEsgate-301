cb8508f88f667711d524fc2068006f4f79ef5e0a
AdrianEsgate-301 <aesgate22@mail.wou.edu>
2025-05-04 14:21:46 -0700

    question 1 submission

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/calcs/<int:x>", methods=["GET"])
+def Decrement_by_one_and_show_hex_number(x):
+    calculations = {"dec": x - 1, "hex": hex(x - 1)}
+    return calculations, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)  # Specify host and port here
+    
+
+
+    
